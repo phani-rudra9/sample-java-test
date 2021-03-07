80e0553a9b4e5765d3ea73b5f3c64536dc37d08a
phani-rudra9 <53288879+phani-rudra9@users.noreply.github.com>
2021-03-08 00:07:04 +0530

    Create ecs-auto.sh

---
+++ ecs-auto.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SERVICE="sample"
+CLUSTER_NAME="sample"
+AWS_REGION="ap-south-1"
+export AWS_PROFILE=default
+
+# Register a new Task definition 
+aws ecs register-task-definition --family sample --cli-input-json file://task-new.json --region $AWS_REGION
+
+# Update Service in the Cluster
+aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE --task-definition sample --desired-count 1 --region $AWS_REGION
